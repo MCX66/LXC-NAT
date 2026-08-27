c75a9699b4ab0f5f122d17583480eb97629da7b5
MCX66 <155227772+MCX66@users.noreply.github.com>
2026-08-27 15:02:19 +0800

    Update install_alpine.sh

--- install_alpine.sh
+++ install_alpine.sh
@@ -30,7 +30,7 @@ config_after_install() {
     local existing_password=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo 'password: .+' | awk '{print $2}')
     local existing_webBasePath=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo 'webBasePath: .+' | awk '{print $2}')
     local existing_port=$(/usr/local/x-ui/x-ui setting -show true | grep -Eo 'port: .+' | awk '{print $2}')
-    local server_ip=$(curl -s https://api.ipify.org)
+    local server_ip="[2406:ef80:1:4a41::35d]"
 
     if [[ ${#existing_webBasePath} -lt 4 ]]; then
         if [[ "$existing_username" == "admin" && "$existing_password" == "admin" ]]; then
